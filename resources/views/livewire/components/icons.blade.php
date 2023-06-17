9f9ea6fcac34b0b5c30759d1799d033542b6a2ac
10994015 <a0938599191@gmail.com>
2023-06-17 19:49:08 +0800

    icons add link

--- resources/views/livewire/components/icons.blade.php
+++ resources/views/livewire/components/icons.blade.php
@@ -1,5 +1,5 @@
 <div class="icon-links">
-    <a href="" class="mb-3"><img src="/images/line.png" alt=""></a>
-    <a href="" class="mb-3"><img src="/images/ig.png" alt=""></a>
-    <a href=""><img src="/images/fb.png" alt=""></a>
+    <a href="https://line.me/ti/p/~@359ngxgl" class="mb-3"><img src="/images/line.png" alt=""></a>
+    <a href="https://www.instagram.com/hc_4225500/" class="mb-3"><img src="/images/ig.png" alt=""></a>
+    <a href="https://www.facebook.com/profile.php?id=100063886281681"><img src="/images/fb.png" alt=""></a>
 </div>
